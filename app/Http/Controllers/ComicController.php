373fca5e64f19fa5c9104496cc0569308df6f594
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-08 19:19:44 +0100

    Add: store function in ComicController

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,7 +36,20 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comic = $request->all();
+
+        $newComic = new Comic();
+        $newComic->title = $comic["title"];
+        $newComic->thumb = $comic["thumb"];
+        $newComic->price = $comic["price"];
+        $newComic->series = $comic["series"];
+        $newComic->sale_date = $comic["sale_date"];
+        $newComic->description = $comic["description"];
+        $newComic->type = $comic["type"];
+
+        $newComic->save();
+
+    return redirect()->route("comics.show", $newComic->id);
     }
 
     /**
