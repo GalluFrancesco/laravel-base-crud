0dcb2594b9fa492e570dfd3743dfa44b2ce136cb
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-03-08 18:50:38 +0100

    Add: comics.show view

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,5 @@
+<div><strong>Titolo</strong>: {{ $comic->title }}</div>
+<div><strong>Prezzo</strong>: {{ $comic->price }}$</div>
+<div><strong>Serie</strong>: {{ $comic->series }}</div>
+<div><strong>Tipo</strong>: {{ $comic->type }}</div>
+
